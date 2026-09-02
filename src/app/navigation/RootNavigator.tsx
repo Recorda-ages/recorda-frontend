4897b0cd9a5bdb6d9f0d0e7b15ccbcde1e50c7e0
Raffaella Tellini Aranha <raffaaranha@hotmail.com>
2026-09-02 19:59:13 -0300

    Salvando as primeiras alteraçãoes da task

--- src/app/navigation/RootNavigator.tsx
+++ src/app/navigation/RootNavigator.tsx
@@ -2,20 +2,29 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { HomeScreen } from "@/features/home/screens/HomeScreen";
+import { OnboardingArtistsScreen } from "@/features/onboarding/screens/OnboardingArtistsScreen";
+import { OnboardingProvider } from "@/features/onboarding/providers/OnboardingContext";
 import { navigationTheme } from "@/theme";
 
 export type RootStackParamList = {
   Home: undefined;
+  OnboardingArtists: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export function RootNavigator() {
   return (
-    <NavigationContainer theme={navigationTheme}>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <OnboardingProvider>
+      <NavigationContainer theme={navigationTheme}>
+        <Stack.Navigator
+          screenOptions={{ headerShown: false, contentStyle: { backgroundColor: "transparent" } }}
+          initialRouteName="OnboardingArtists"
+        >
+          <Stack.Screen name="OnboardingArtists" component={OnboardingArtistsScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </OnboardingProvider>
   );
 }
