12db6b470191ef44fbf05c5e765081434444e8b7
Raffaella Tellini Aranha <raffaaranha@hotmail.com>
2026-09-02 21:55:51 -0300

    ajuste design parte 1

--- src/features/onboarding/providers/OnboardingContext.tsx
+++ src/features/onboarding/providers/OnboardingContext.tsx
@@ -45,7 +45,7 @@ export function OnboardingProvider({ children }: { children: ReactNode }) {
         addArtist,
         removeArtist,
         toggleArtist,
-        clearArtists,
+        clearArtists
       }}
     >
       {children}
