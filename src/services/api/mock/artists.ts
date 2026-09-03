9761296f4e26740c646e2ae975805db090355b00
Raffaella Tellini Aranha <raffaaranha@hotmail.com>
2026-09-02 22:28:59 -0300

    feat: Tela onboarding Artistas sem gradiente

--- src/services/api/mock/artists.ts
+++ src/services/api/mock/artists.ts
@@ -4,52 +4,52 @@ const MOCK_ARTISTS: Artist[] = [
   {
     id: "1",
     name: "The Beatles",
-    imageUrl: "https://i.scdn.co/image/ab6761610000e5ebe9348cc01ff5d55971b22433"
+    imageUrl: "https://picsum.photos/seed/beatles/200/200"
   },
   {
     id: "2",
     name: "Queen",
-    imageUrl: "https://i.scdn.co/image/ab6761610000e5eb2ddb3d7a8d11634625b6a378"
+    imageUrl: "https://picsum.photos/seed/queen/200/200"
   },
   {
     id: "3",
     name: "David Bowie",
-    imageUrl: "https://i.scdn.co/image/ab6761610000e5eb0092c4d9bc2133a8a30ef1d4"
+    imageUrl: "https://picsum.photos/seed/bowie/200/200"
   },
   {
     id: "4",
     name: "Pink Floyd",
-    imageUrl: "https://i.scdn.co/image/ab6761610000e5eb8ac052ed34e2b02ea4280cf1"
+    imageUrl: "https://picsum.photos/seed/floyd/200/200"
   },
   {
     id: "5",
     name: "Radiohead",
-    imageUrl: "https://i.scdn.co/image/ab6761610000e5eb38cc84a44b150965bc93b169"
+    imageUrl: "https://picsum.photos/seed/radiohead/200/200"
   },
   {
     id: "6",
     name: "Nirvana",
-    imageUrl: "https://i.scdn.co/image/ab6761610000e5eb1d6ebfa773199d98cb2ecf57"
+    imageUrl: "https://picsum.photos/seed/nirvana/200/200"
   },
   {
     id: "7",
     name: "Metallica",
-    imageUrl: "https://i.scdn.co/image/ab6761610000e5ebc14c330f6580e03be81f9cf1"
+    imageUrl: "https://picsum.photos/seed/metallica/200/200"
   },
   {
     id: "8",
     name: "Led Zeppelin",
-    imageUrl: "https://i.scdn.co/image/ab6761610000e5ebd7a4f9f7c0064eb6f3933c06"
+    imageUrl: "https://picsum.photos/seed/zeppelin/200/200"
   },
   {
     id: "9",
     name: "Arctic Monkeys",
-    imageUrl: "https://i.scdn.co/image/ab6761610000e5eb7da39dea0a72f581535fb11f"
+    imageUrl: "https://picsum.photos/seed/arctic/200/200"
   },
   {
     id: "10",
     name: "Foo Fighters",
-    imageUrl: "https://i.scdn.co/image/ab6761610000e5eb38d2f1f50a80dc24637e19b3"
+    imageUrl: "https://picsum.photos/seed/foo/200/200"
   }
 ];
 
